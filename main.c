cf5ae130ab3fb6961fd9f21b9b259318fb48e1c3
haithemyk <haithemyk0707@gmail.com>
2023-05-25 18:56:15 +0100

    sw

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
  * main - the entry of the shell.
  * Return: 0 if the success of the program.
 */
-int main(void)
+int main(int ac, char **av, char **env)
 {
 	char *command;
 	size_t buffer_size;
 	size_t characters;
 	char **argv;
-	char **env;
-
+	unsigned int i = 0;
 	while (1)
 	{
+		printf("%s",av[0]);
 		printf("$ ");
 		fflush(stdout);
 		characters = getline(&command, &buffer_size, stdin);
@@ -22,20 +22,20 @@ int main(void)
 			break;
 		}
 		argv = command_spliter(command);
-		if (strcmp(argv[0], "exit") == 0)
+		if (strcmp(argv[1], "exit") == 0)
 			exit(0);
-		if (strcmp(argv[0], "env") == 0)
+		if (strcmp(argv[1], "env") == 0)
 		{
-			while (*env != NULL)
+			i = 0;
+			while (env[i] != NULL)
 			{
-				printf("%s\n", *env);
-				env++;
+				printf("%s\n", env[i++]);
 			}
 		}
 		if (characters != 0)
 		{
 			if (execute(argv) == 1)
-				printf("No such file or directory\n");
+				printf("%s:No such file or directory\n",av[ac - 1]);
 		}
 	}
 	free(command);
